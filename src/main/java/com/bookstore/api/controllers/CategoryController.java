a31a58fbb033ae720c7c3f6ead65c1a8ab887dd2
Emirhan Dogandemir <emirhandgndmr51@gmail.com>
2021-07-29 13:08:02 +0300

    Category and Customer added (Controller)

--- src/main/java/com/bookstore/api/controllers/CategoryController.java
+++ src/main/java/com/bookstore/api/controllers/CategoryController.java
@@ -1,5 +1,53 @@
 package com.bookstore.api.controllers;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bookstore.business.abstracts.CategoryService;
+import com.bookstore.core.utilities.results.DataResult;
+import com.bookstore.core.utilities.results.Result;
+import com.bookstore.domain.Category;
+
+@RestController
+@RequestMapping("/api/categories")
 public class CategoryController {
 
+	private CategoryService categoryService;
+
+	@Autowired
+	public CategoryController(CategoryService categoryService) {
+		super();
+		this.categoryService = categoryService;
+	}
+	
+	public ResponseEntity<?> add(@Valid @RequestBody Category category){
+		return ResponseEntity.ok(this.categoryService.add(category));
+	}
+	public ResponseEntity<?> update(@Valid @RequestBody Category category){
+		return ResponseEntity.ok(this.categoryService.update(category));
+	}
+	
+	public Result delete(@RequestParam int id) {
+		return this.categoryService.delete(id);
+	}
+	
+	public DataResult<Category> getBYId(@RequestParam int id){
+		return this.categoryService.getById(id);
+	}
+	public DataResult<Category> getByName(@RequestParam String name){
+		return this.categoryService.getByName(name);
+	}
+	public DataResult<List<Category>> getAll(){
+		return this.categoryService.getAll();
+	}
+	
+	
 }
--- src/main/java/com/bookstore/api/controllers/CustomerController.java
+++ src/main/java/com/bookstore/api/controllers/CustomerController.java
@@ -1,5 +1,55 @@
 package com.bookstore.api.controllers;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bookstore.business.abstracts.CustomerService;
+import com.bookstore.core.utilities.results.DataResult;
+import com.bookstore.core.utilities.results.Result;
+import com.bookstore.domain.Customer;
+
+@RestController
+@RequestMapping("/api/customers")
 public class CustomerController {
 
+	private CustomerService customerService;
+
+	@Autowired
+	public CustomerController(CustomerService customerService) {
+		super();
+		this.customerService = customerService;
+	}
+	
+	public ResponseEntity<?> add(@Valid @RequestBody Customer customer){
+		return ResponseEntity.ok(this.customerService.add(customer));
+	}
+	public ResponseEntity<?> update(@Valid @RequestBody Customer customer){
+		return ResponseEntity.ok(this.customerService.update(customer));
+	}
+	public Result delete(@RequestParam int id) {
+		return this.customerService.delete(id);
+	}
+	public DataResult<Customer> getById(@RequestParam int customerId){
+		return this.customerService.getById(customerId);
+	}
+	public DataResult<Customer> getByFirstName(@RequestParam String firstName){
+		return this.customerService.getByFirstName(firstName);
+	}
+	public DataResult<Customer> getByLastName(@RequestParam String lastName){
+		return this.customerService.getByLastName(lastName);
+	}
+	public DataResult<Customer> getByPhoneNumber(@RequestParam String phoneNumber){
+		return this.customerService.getByPhoneNumber(phoneNumber);
+	}
+	public DataResult<List<Customer>> getAll(){
+		return this.customerService.getAll();
+	}
 }
