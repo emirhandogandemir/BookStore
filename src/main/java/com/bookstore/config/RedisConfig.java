b6f46b0a852a5a606f3734bed3b6ef1720d1fc12
Emirhan Dogandemir <emirhandgndmr51@gmail.com>
2021-08-07 19:56:01 +0300

    redis configuration added

--- src/main/java/com/bookstore/config/RedisConfig.java
+++ src/main/java/com/bookstore/config/RedisConfig.java
@@ -1,5 +1,32 @@
 package com.bookstore.config;
 
-public class RedisConfig {
+import java.time.Duration;
 
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+	@Bean
+	public RedisCacheConfiguration cacheConfiguration() {
+	    return RedisCacheConfiguration.defaultCacheConfig()
+	      .entryTtl(Duration.ofMinutes(60))
+	      .disableCachingNullValues()
+	      .serializeValuesWith(SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+	}
+	
+	@Bean
+	public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
+	    return (builder) -> builder
+	      .withCacheConfiguration("itemCache",
+	        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(10)))
+	      .withCacheConfiguration("customerCache",
+	        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(5)));
+	}
 }
--- src/main/java/com/bookstore/domain/dtos/UserDto.java
+++ src/main/java/com/bookstore/domain/dtos/UserDto.java
@@ -6,11 +6,15 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
 
 	private String email;
