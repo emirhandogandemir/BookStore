bbc6862ed0398d0a248d96f3fc3f0602257c3bf8
Emirhan Dogandemir <emirhandgndmr51@gmail.com>
2021-08-19 11:01:15 +0300

    Access Token and SecretKey has been created

--- src/main/java/com/bookstore/core/utilities/security/AccessToken.java
+++ src/main/java/com/bookstore/core/utilities/security/AccessToken.java
@@ -1,5 +1,17 @@
 package com.bookstore.core.utilities.security;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccessToken {
 
+	private String token;
+	
 }
--- src/main/java/com/bookstore/core/utilities/security/SecretKey.java
+++ src/main/java/com/bookstore/core/utilities/security/SecretKey.java
@@ -1,5 +1,17 @@
 package com.bookstore.core.utilities.security;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SecretKey {
 
+	private String secretKey;
+	private long expirationInMiliseconds;
+	
 }
